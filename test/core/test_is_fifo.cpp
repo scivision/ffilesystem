b047967e4761080e7694c2d311d9451918561feb
scivision <scivision@users.noreply.github.com>
2025-01-31 10:05:15 -0500

    test: print location in source where failure occured, if available

--- test/core/test_is_fifo.cpp
+++ test/core/test_is_fifo.cpp
@@ -5,14 +5,67 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
-int main(int argc, char* argv[])
+#if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#else
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h> // getpid
+#endif
+
+#if __has_include(<format>)
+#include <format>
+#endif
+
+
+int main()
 {
 
-const std::string p = (argc > 1) ? argv[1] : argv[0];
+std::string name;
 
-if (fs_is_fifo(p))
-  err("is_fifo(" + p + ") should be false");
+#if defined(_WIN32)
+// https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-createnamedpipea
+
+// must have this path prefix or INVALID_HANDLE_VALUE results
+#ifdef __cpp_lib_format  // C++20
+  name = std::format(R"(\\.\pipe\test_pipe_{})", GetCurrentProcessId());
+#else
+  name = R"(\\.\pipe\test_pipe_)" + std::to_string(GetCurrentProcessId());
+#endif
+
+HANDLE hPipe = CreateNamedPipeA(name.data(),
+                             PIPE_ACCESS_DUPLEX,
+                             PIPE_TYPE_BYTE,
+                             1,
+                             0, 0, 0, nullptr);
+
+if (hPipe == INVALID_HANDLE_VALUE)
+  err("test_is_fifo:CreateNamedPipeA(" + name + ")");
+
+#else
+
+#ifdef __cpp_lib_format
+  name = std::format("test_pipe_{}", getpid());
+#else
+  name = "test_pipe_" + std::to_string(getpid());
+#endif
+
+  if (mkfifo(name.c_str(), 0666) == -1)
+    err("mkfifo(" + name + ")");
+#endif
+
+if (!fs_is_fifo(name))
+  err("is_fifo(" + name + ") should be true");
+
+#if defined(_WIN32)
+if (!CloseHandle(hPipe))
+  err("CloseHandle(" + name + ")");
+#endif
+
+ok_msg("is_fifo");
 
 return EXIT_SUCCESS;
 
--- test/ffilesystem_test.h
+++ test/ffilesystem_test.h
@@ -2,18 +2,38 @@
 #include <iostream>
 #include <string_view>
 
+#if defined(_WIN32) || defined(__CYGWIN__)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#else
 #include <cerrno>
+#endif
 
+#if __has_include(<source_location>)
+#include <source_location>
+#endif
 
 [[noreturn]]
-void err(std::string_view m){
-  std::cerr << "FAILED: " << m << " backend: " << fs_backend();
+void err(std::string_view m
+#if defined(__cpp_lib_source_location)
+ , const std::source_location& location = std::source_location::current()){
+  std::cerr << "FAILED: " << m << " at " << location.file_name() << ":" << location.line();
+#else
+  ){
+  std::cerr << "FAILED: " << m;
+#endif
 
+  std::cerr << " backend: " << fs_backend() << "\n";
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+  DWORD error = GetLastError();
+  std::cerr << std::system_category().message(error) << "\n";
+#else
   if(errno){
-  auto econd = std::generic_category().default_error_condition(errno);
-
-  std::cerr << " " << econd.message();
+    auto econd = std::generic_category().default_error_condition(errno);
+    std::cerr << " " << econd.message();
   }
+#endif
 
   std::cerr << std::endl;
 
