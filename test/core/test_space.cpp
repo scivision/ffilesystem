c73b9186fb5065ec148c656aee73ca7268dd95a2
scivision <scivision@users.noreply.github.com>
2025-02-14 15:59:35 -0500

    GTest:space

--- test/core/test_space.cpp
+++ test/core/test_space.cpp
@@ -1,28 +1,11 @@
-#include <iostream>
-#include <cstdlib>
-
-#include <string>
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
 
+#include <gtest/gtest.h>
 
-int main(int argc, char* argv[])
+TEST(TestSpace, Space)
 {
 
-const std::string s1 = (argc > 1) ? argv[1] : fs_parent(argv[0]);
+EXPECT_GT(fs_space_available(testing::TempDir()), 0);
+EXPECT_GT(fs_space_capacity(testing::TempDir()), 0);
 
-std::uintmax_t avail = fs_space_available(s1);
-std::uintmax_t cap = fs_space_capacity(s1);
-
-if (avail == 0)
-  err("space_available(" + s1 + ") " + std::to_string(avail));
-if (cap == 0)
-  err("space_capacity(" + s1 + ") " + std::to_string(cap));
-
-
-ok_msg("space_available (GB): " + std::to_string(static_cast<double>(avail) / static_cast<double>(1024^3)));
-ok_msg("space_capacity (GB): " + std::to_string(static_cast<double>(cap) / static_cast<double>(1024^3)));
-
-return EXIT_SUCCESS;
 }
