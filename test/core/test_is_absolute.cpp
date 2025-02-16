83e823ed1989539e4e6e8fddd80ea7d2aef18787
scivision <scivision@users.noreply.github.com>
2025-02-16 16:02:08 -0500

    GTest:IsAbs

--- test/core/test_is_absolute.cpp
+++ test/core/test_is_absolute.cpp
@@ -1,64 +1,27 @@
-#include <iostream>
-#include <cstdlib>
-
-#ifdef _MSC_VER
-#include <crtdbg.h>
-#endif
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
+
+#include <gtest/gtest.h>
 
 
-int main()
+TEST(IsAbs,IsAbs)
 {
-#ifdef _MSC_VER
-_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
-_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
-#endif
 
-if (fs_is_absolute("")) {
-  std::cerr << "blank is not absolute\n";
-  return EXIT_FAILURE;
-}
+EXPECT_FALSE(fs_is_absolute(""));
 
 if(fs_is_windows()){
-  if (!fs_is_absolute("J:/"))
-    err("is_absolute(J:/) on Windows should be true");
-
-  if (!fs_is_absolute("j:/"))
-    err("is_absolute(j:/) on Windows should be true");
-
-  if (fs_is_absolute("j:"))
-    err("is_absolute(j:) on Windows should be false");
-
-  if (fs_is_absolute("/"))
-    err("is_absolute(/) on Windows should be false");
-
-  if (fs_is_absolute("/日本語"))
-    err("is_absolute(/日本語) on Windows should be false");
+  EXPECT_TRUE(fs_is_absolute("J:/"));
+  EXPECT_TRUE(fs_is_absolute("j:/"));
+  EXPECT_FALSE(fs_is_absolute("j:"));
+  EXPECT_FALSE(fs_is_absolute("/"));
+  EXPECT_FALSE(fs_is_absolute("/日本語"));
 
 } else {
-  if (!fs_is_absolute("/"))
-    err("is_absolute(/) on Unix should be true");
-
-  if (!fs_is_absolute("/日本語"))
-    err("is_absolute(/日本語) should be true");
-
-  if (fs_is_absolute("j:/"))
-    err("is_absolute(j:/) on Unix should be false");
+  EXPECT_TRUE(fs_is_absolute("/"));
+  EXPECT_TRUE(fs_is_absolute("/日本語"));
+  EXPECT_FALSE(fs_is_absolute("j:/"));
 }
 
-  if (fs_is_absolute("日本語"))
-    err("is_absolute(日本語) should be false");
+  EXPECT_FALSE(fs_is_absolute("日本語"));
+  EXPECT_FALSE(fs_is_absolute("some space here"));
 
-  if (fs_is_absolute("some space here"))
-    err("is_absolute(some space here) should be false");
-
-  ok_msg("is_absolute C++");
-
-  return EXIT_SUCCESS;
 }
