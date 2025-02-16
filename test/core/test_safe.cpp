caa947a1c9e2d403d0a7ca026a53bae3160f7124
scivision <scivision@users.noreply.github.com>
2025-02-16 15:55:45 -0500

    GTest:IsSafe

--- test/core/test_safe.cpp
+++ test/core/test_safe.cpp
@@ -1,53 +1,20 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-
-#ifdef _MSC_VER
-#include <crtdbg.h>
-#endif
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
 
+#include <gtest/gtest.h>
 
-int main(){
-#ifdef _MSC_VER
-_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
-_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
-#endif
-/// is safe name expects ONLY the filename, not the path
+TEST(TestSafe, Safe)
+{
 
-std::string s;
+EXPECT_FALSE(fs_is_safe_name("test/re/"));
+EXPECT_FALSE(fs_is_safe_name("test/re"));
 
-s = "test/re/";
+if(fs_is_windows())
+  EXPECT_FALSE(fs_is_safe_name("hi."));
+else
+  EXPECT_TRUE(fs_is_safe_name("hi."));
 
-if(fs_is_safe_name(s))
-  err(s);
+EXPECT_FALSE(fs_is_safe_name("hi there"));
 
-s = "test/re";
-if(fs_is_safe_name(s))
-  err(s);
+EXPECT_FALSE(fs_is_safe_name("日本語"));
 
-s = "hi.";
-bool ok = fs_is_safe_name(s);
-if(fs_is_windows() && ok)
-  err(s);
-if(!fs_is_windows() && !ok)
-  err(s);
-
-s = "hi there";
-if(fs_is_safe_name(s))
-  err(s);
-
-s = "日本語";
-if(fs_is_safe_name(s))
-  err(s);
-
-ok_msg("safe C++");
-
-return EXIT_SUCCESS;
 }
