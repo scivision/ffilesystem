8962c165d13ce13f7f97cea85fb60ea04c41e6af
scivision <scivision@users.noreply.github.com>
2025-02-17 01:52:09 -0500

    Gtest: is{prefix,subdir}

--- test/core/test_is_subdir.cpp
+++ test/core/test_is_subdir.cpp
@@ -1,40 +1,17 @@
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <string_view>
-#include <string>
-#include <tuple>
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
 
+#include <gtest/gtest.h>
 
-int main()
+TEST(TestSubdir, IsSubdir)
 {
 
-std::vector<std::tuple<std::string_view, std::string_view, bool>> test_cases = {
-    {"a/b/c", "a/b", true}, {"a/b/c", "a/b/", true}, {"a/b/c", "a", true},
-    {"a/b", "a/b", false}, {"a/b", "a/b/", false},
-    {"a/b", "c", false}, {"b", "a/b", false},
-    {"c:/a", "c:/", true}
+EXPECT_EQ(fs_is_subdir("a/b/c", "a/b"), true);
+EXPECT_EQ(fs_is_subdir("a/b/c", "a/b/"), true);
+EXPECT_EQ(fs_is_subdir("a/b/c", "a"), true);
+EXPECT_EQ(fs_is_subdir("a/b", "a/b"), false);
+EXPECT_EQ(fs_is_subdir("a/b", "a/b/"), false);
+EXPECT_EQ(fs_is_subdir("a/b", "c"), false);
+EXPECT_EQ(fs_is_subdir("b", "a/b"), false);
+EXPECT_EQ(fs_is_subdir("c:/a", "c:/"), true);
 
-};
-
-int fail = 0;
-
-    for (const auto& [s1, s2, expected] : test_cases) {
-        if (fs_is_subdir(s1, s2) != expected) {
-            std::cerr << "FAIL: " << s1 << " is " << (expected ? "not " : "") << "subdir of " << s2 << "\n";
-            fail++;
-        }
-    }
-
-    if(fail){
-        std::cerr << "FAIL: fs_is_subdir " << fail << "failures.\n";
-        return EXIT_FAILURE;
-    }
-
-    ok_msg("fs_is_subdir");
-
-    return EXIT_SUCCESS;
 }
