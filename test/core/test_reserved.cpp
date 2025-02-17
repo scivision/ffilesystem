e8087fb9c2e79b73bff99cef7e9061bd8fbacf13
scivision <scivision@users.noreply.github.com>
2025-02-17 01:29:15 -0500

    GTest:Reserved

--- test/core/test_reserved.cpp
+++ test/core/test_reserved.cpp
@@ -1,118 +1,66 @@
-#include <cstdlib>
-#include <cstdint>
-#include <iostream>
 #include <string>
 
-#ifdef _MSC_VER
-#include <crtdbg.h>
-#endif
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
 
+#include <gtest/gtest.h>
 
-int main(){
+TEST(TestReserved, Reserved)
+{
 
-#ifdef _MSC_VER
-  _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
-#endif
+std::string const ref(fs_devnull());
 
-    const std::string ref(fs_devnull());
+EXPECT_FALSE(fs_is_reserved("."));
 
-    std::string r;
+EXPECT_EQ(fs_normal(ref), ref);
 
-    std::cout << "Begin test_reserved\n";
+bool b = fs_is_absolute(ref);
+if (fs_is_windows())
+  EXPECT_FALSE(b);
+else
+  EXPECT_TRUE(b);
 
-    if (fs_is_reserved("."))
-      err("is_reserved(.) should be false");
+b = fs_is_reserved(ref);
+if (fs_is_windows())
+  EXPECT_TRUE(b);
+else
+  EXPECT_FALSE(b);
 
-    r = fs_normal(ref);
-    if (r != ref)
-      err("normal");
-    std::cout << "OK: normal(" << r << ")\n";
+EXPECT_FALSE(fs_is_dir(ref));
 
-    bool b = fs_is_absolute(ref);
-    if (fs_is_windows()){
-      if(b) err("is_absolute");
-    }
-    else{
-      if(!b) err("is_absolute");
-    }
-    std::cout << "OK: is_absolute(" << r << ")\n";
+EXPECT_EQ(fs_file_size(ref), 0);
 
-    b = fs_is_reserved(ref);
-    if(fs_is_windows()){
-      if(!b) err("is_reserved");
-    }
-    else{
-      if(b) err("is_reserved");
-    }
-    std::cout << "OK: is_reserved(" << r << ")\n";
-
-    if(fs_is_dir(ref))
-      err("is_dir");
-    std::cout << "OK: is_dir(" << r << ")\n";
-
-    std::uintmax_t s = fs_file_size(ref);
-    if(s != 0){
-      std::cerr << "FAILED: file_size(" << ref << ") " << s << "\n";
-      err("file_size");
-    }
-    std::cout << "OK: file_size() " << s << "\n";
-
-    // omitted fs_space_available() since some systems don't handle NUL /dev/null well
-    // e.g. Windows, macOS GCC, etc.
+// omitted fs_space_available() since some systems don't handle NUL /dev/null well
+// e.g. Windows, macOS GCC, etc.
 
 
 if(!fs_is_windows()){
 
-    if(fs_is_exe(ref))
-      err("is_exe");
+EXPECT_FALSE(fs_is_exe(ref));
 
-    // NOTE: do not test
-    //
-    // create_directories(/dev/null)
-    // remove(/dev/null)
-    // create_symlink()
-    // set_permissionss()
-    //
-    // since if testing with "root" privilidges,
-    // it can make the system unusable until reboot!
+// NOTE: do not test
+//
+// create_directories(/dev/null)
+// remove(/dev/null)
+// create_symlink()
+// set_permissionss()
+//
+// since if testing with "root" privilidges,
+// it can make the system unusable until reboot!
 
-    if(!fs_exists(ref))
-      err("exists");
-    std::cout << "OK: exists(" << r << ")\n";
+EXPECT_TRUE(fs_exists(ref));
 
-    if(fs_is_file(ref))
-      err("is_file");
-    std::cout << "OK: is_file(" << r << ")\n";
+EXPECT_FALSE(fs_is_file(ref));
 
-    if(fs_canonical(ref.data(), false, true).empty())
-      err("canonical");
-    std::cout << "OK: canonical() " << ref << "\n";
+EXPECT_FALSE(fs_canonical(ref, false, true).empty());
 
-} // fs_is_windows()
+} // !fs_is_windows()
 
-    r = fs_expanduser(ref);
-    if(r != ref)
-      err("expanduser");
+EXPECT_EQ(fs_expanduser(ref), ref);
 
-    if(fs_copy_file(ref, ref, false))
-      err("copy_file");
-    std::cout << "OK: copy_file() " << ref << "\n";
+EXPECT_FALSE(fs_copy_file(ref, ref, false));
 
-    // touch is ambiguous on reserved, so omit
+// touch is ambiguous on reserved, so omit
 
-    if(fs_is_symlink(ref))
-      err("is_symlink");
-    std::cout << "OK: is_symlink() " << r << "\n";
+EXPECT_FALSE(fs_is_symlink(ref));
 
-    ok_msg("reserved C++");
-
-    return EXIT_SUCCESS;
 }
