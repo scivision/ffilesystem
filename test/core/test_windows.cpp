7126360a475dc1d637934f0c05f17f05df54e1e2
scivision <scivision@users.noreply.github.com>
2025-02-16 19:08:27 -0500

    GTest:Windows

--- test/core/test_windows.cpp
+++ test/core/test_windows.cpp
@@ -1,48 +1,18 @@
-#include <cstdlib>
-#include <iostream>
 #include <string>
 
-#ifdef _MSC_VER
-#include <crtdbg.h>
-#endif
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
 
+#include <gtest/gtest.h>
 
-int main(int argc, char** argv)
+TEST(TestWindows, ShortLong)
 {
 
-#ifdef _MSC_VER
-_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
-_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
-#endif
-
-std::string long_path = (argc < 2) ? fs_getenv("PROGRAMFILES") : argv[1];
-
-if (long_path.empty())
-  err("input is empty");
-
+std::string long_path = fs_getenv("PROGRAMFILES");
 std::string short_path = fs_shortname(long_path);
 
-std::cout << long_path << " => " << short_path << '\n';
-if(short_path.empty())
-  err("short_path is empty");
+ASSERT_FALSE(long_path.empty());
+ASSERT_FALSE(short_path.empty());
 
-std::string long_path2 = fs_longname(short_path);
+EXPECT_EQ(fs_longname(short_path), long_path);
 
-std::cout << short_path << " => " << long_path2 << '\n';
-if(long_path2.empty())
-  err("long_path is empty");
-
-if (long_path != long_path2)
-  err("long_path != long_path2");
-
-ok_msg("shortname and longname C++");
-
-return EXIT_SUCCESS;
 }
