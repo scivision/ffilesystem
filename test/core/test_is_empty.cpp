17aa1ee5b5549b8d723b97bae20456d2343684b8
scivision <scivision@users.noreply.github.com>
2025-02-17 20:06:44 -0500

    GTest:isempty

--- test/core/test_is_empty.cpp
+++ test/core/test_is_empty.cpp
@@ -1,39 +1,24 @@
-#include <iostream>
-#include <cstdlib>
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
+#include <iostream>
 
-int main(){
+#include <gtest/gtest.h>
 
-std::string f;
+class TestEmpty : public testing::Test {
+    protected:
+      std::string dir;
+      void SetUp() override {
+        dir = testing::TempDir() + "/empty_dir";
+        ASSERT_TRUE(fs_mkdir(dir));
+      }
+      void TearDown() override {
+        std::remove(dir.c_str());
+      }
+    };
 
-if (fs_is_empty("."))
-  err("is_empty(.) should be false generally");
+TEST_F(TestEmpty, IsEmpty)
+{
+    EXPECT_FALSE(fs_is_empty("."));
+    EXPECT_TRUE(fs_is_empty(dir));
 
-
-f = "test_is_empty_cpp_dir";
-if(!fs_mkdir(f))
-  err("Failed mkdir " + f);
-
-if (!fs_is_empty(f))
-  err("is_empty(" + f + ") should be true");
-
-
-f = "test_is_empty_cpp.txt";
-if(!fs_touch(f))
-  err("Failed to touch " + f);
-
-if (!fs_is_empty(f))
-  err("is_empty(" + f + ") should be true");
-
-
-f = "not-exist-is-empty_cpp";
-if (fs_is_empty(f))
-  err("is_empty(" + f + ") should be false");
-
-ok_msg("is_empty C++");
-
-return EXIT_SUCCESS;
-
+    EXPECT_FALSE(fs_is_empty(dir + "/not-exist-is-empty_cpp"));
 }
