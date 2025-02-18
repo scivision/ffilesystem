6e5a9bb256da3b76c83cef15b13a97dbf37ea0a0
scivision <scivision@users.noreply.github.com>
2025-02-17 20:26:05 -0500

    GTest:hardLink

--- test/core/test_hard_link.cpp
+++ test/core/test_hard_link.cpp
@@ -1,20 +1,8 @@
-#include <iostream>
-
-#include <cstdlib>
-#include <cstdint>
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
 
+#include <gtest/gtest.h>
 
-int main() {
-
-  std::uintmax_t c = fs_hard_link_count(".");
-
-  if(c < 1)
-    err("hard_link_count: " + std::to_string(c));
-
-  ok_msg("hard_link_count C++");
-
-  return EXIT_SUCCESS;
+TEST(TestHardLink, HardLink)
+{
+EXPECT_GE(fs_hard_link_count("."), 1);
 }
