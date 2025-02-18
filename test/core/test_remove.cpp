0af685c42d7013eb46adfa8b304baada9ae7ee65
scivision <scivision@users.noreply.github.com>
2025-02-17 20:23:39 -0500

    GTest:remove

--- test/core/test_remove.cpp
+++ test/core/test_remove.cpp
@@ -1,29 +1,22 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <string_view>
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
+
+#include <gtest/gtest.h>
 
 
-int main()
+class TestRemove : public testing::Test {
+  protected:
+    std::string file;
+    void SetUp() override {
+      file = testing::TempDir() + "/test_remove.txt";
+      ASSERT_TRUE(fs_touch(file));
+    }
+    void TearDown() override {
+      std::remove(file.c_str());
+    }
+  };
+
+
+TEST_F(TestRemove, Remove)
 {
-
-  const std::string j = "test_remove.txt";
-
-  if(!fs_touch(j))
-    err("touch failed for " + j);
-  if(!fs_is_file(j))
-    err(j + " is not a file");
-
-  if(!fs_remove(j)){
-    if(fs_exists(j))
-      err("remove(" + j + ") failed--file still exists");
-    err("remove(" + j + ") failed");
-  }
-
-  ok_msg("remove(" + j + ") C++");
-
-  return EXIT_SUCCESS;
+ASSERT_TRUE(fs_remove(file));
 }
