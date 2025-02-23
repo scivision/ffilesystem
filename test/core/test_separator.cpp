7f95ab1bdcccdf8a94b6af10070da1f96391d354
scivision <scivision@users.noreply.github.com>
2025-02-23 16:34:00 -0500

    GTest:Separator

--- test/core/test_separator.cpp
+++ test/core/test_separator.cpp
@@ -1,52 +1,24 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-
-#ifdef _MSC_VER
-#include <crtdbg.h>
-#endif
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
 
-void test_as_posix(){
+#include <gtest/gtest.h>
 
-  std::string p;
+TEST(TestSeparator, AsPosix)
+{
 
-  if(!fs_as_posix(p).empty())
-    err("test_as_posix: " + p);
+EXPECT_TRUE(fs_as_posix("").empty());
 
-  if(fs_is_windows()){
-    p = "a\\b";
-    if(fs_as_posix(p) != "a/b")
-      err("test_as_posix: " + fs_as_posix(p) + " != a/b");
-
-    p = R"(C:\my\path)";
-    if(fs_as_posix(p) != "C:/my/path")
-      err("test_as_posix: " + p);
-  }
-
-  std::cout << "OK: as_posix\n";
+if(fs_is_windows()){
+EXPECT_EQ(fs_as_posix("a\\b"), "a/b");
+EXPECT_EQ(fs_as_posix(R"(C:\my\path)"), "C:/my/path");
 }
 
+}
 
-int main() {
+TEST(TestSeparator, PathSep) {
 
-#ifdef _MSC_VER
-  _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
-#endif
-
-  test_as_posix();
-
-  if(fs_is_windows() && fs_pathsep() != ';')
-    err("pathsep windows");
-  if(!fs_is_windows() && fs_pathsep() != ':')
-    err("pathsep unix");
-
-  return EXIT_SUCCESS;
+if(fs_is_windows()){
+  EXPECT_EQ(fs_pathsep(), ';');
+} else {
+  EXPECT_EQ(fs_pathsep(), ':');
+}
 }
