b86796bb18878f5e99515967c2f0f63f7c00fbc3
scivision <scivision@users.noreply.github.com>
2025-03-09 10:49:24 -0400

    test:consolidate

--- test/core/test_OnDisk.cpp
+++ test/core/test_OnDisk.cpp
@@ -6,8 +6,10 @@
 
 class TestOnDisk : public testing::Test {
   protected:
-    std::string file, dir;
+    std::string file, dir, cwd;
     void SetUp() override {
+      cwd = fs_get_cwd();
+
       file = "ffs_is_file_empty.txt";
       ASSERT_TRUE(fs_touch(file));
       ASSERT_TRUE(fs_exists(file));
@@ -27,6 +29,8 @@ TEST_F(TestOnDisk, IsDir)
 {
   EXPECT_FALSE(fs_is_dir(""));
   EXPECT_TRUE(fs_is_dir("."));
+  EXPECT_TRUE(fs_is_dir(cwd));
+  EXPECT_TRUE(fs_is_dir(dir));
   EXPECT_TRUE(fs_is_readable("."));
   EXPECT_FALSE(fs_is_dir(file));
   EXPECT_FALSE(fs_is_dir("ffs_is_dir_not-exist-dir"));
@@ -40,6 +44,8 @@ TEST_F(TestOnDisk, IsFile){
   EXPECT_FALSE(fs_is_file("ffs_is_file_not-exist-file"));
   EXPECT_FALSE(fs_is_file(""));
   EXPECT_FALSE(fs_is_file("."));
+  EXPECT_FALSE(fs_is_file(dir));
+  EXPECT_FALSE(fs_is_file(cwd));
 }
 
 
@@ -48,6 +54,7 @@ TEST_F(TestOnDisk, IsOther){
   EXPECT_FALSE(fs_is_other("."));
   EXPECT_FALSE(fs_is_other(file));
   EXPECT_FALSE(fs_is_other(dir));
+  EXPECT_FALSE(fs_is_other(cwd));
   EXPECT_FALSE(fs_is_other("ffs_is_other_not-exist-file"));
 }
 
@@ -64,4 +71,30 @@ ASSERT_TRUE(fs_set_cwd(dir));
 
 EXPECT_TRUE(fs_mkdir("test-filesystem-dir/hello"));
 EXPECT_TRUE(fs_is_dir(fs_get_cwd() + "/test-filesystem-dir/hello"));
+}
+
+
+TEST_F(TestOnDisk, Realpath){
+
+EXPECT_EQ(fs_realpath("."), cwd);
+EXPECT_TRUE(fs_realpath("not-exist-realpath/b/c").empty());
+
+EXPECT_LT(fs_realpath("..").length(), cwd.length());
+}
+
+
+TEST_F(TestOnDisk, Touch)
+{
+
+EXPECT_TRUE(fs_touch(file));
+
+auto t0 = fs_get_modtime(file);
+EXPECT_GT(t0, 0);
+
+EXPECT_TRUE(fs_set_modtime(file));
+
+EXPECT_GE(fs_get_modtime(file), t0);
+
+EXPECT_FALSE(fs_set_modtime("not-exist-file"));
+
 }
