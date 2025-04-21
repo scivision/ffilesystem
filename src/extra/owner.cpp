263b849066427862ff11ae2fed93d264e8ede4a8
scivision <scivision@users.noreply.github.com>
2025-04-21 14:03:53 -0400

    owner: windows: functionalize

--- src/extra/owner.cpp
+++ src/extra/owner.cpp
@@ -29,6 +29,7 @@
 
 
 #if defined(_WIN32)
+
 static std::string fs_win32_get_owner(PSID pSid)
 {
   DWORD L1 = 0;
@@ -46,11 +47,34 @@ static std::string fs_win32_get_owner(PSID pSid)
   s.resize(L1);
   return s;
 }
+
+static std::string fs_win32_owner(std::string_view path, bool group)
+{
+// https://learn.microsoft.com/en-us/windows/win32/secauthz/finding-the-owner-of-a-file-object-in-c--
+
+  PSECURITY_DESCRIPTOR pSD = nullptr;
+  PSID pSid = nullptr;
+  DWORD r;
+  // https://learn.microsoft.com/en-us/windows/win32/api/aclapi/nf-aclapi-getnamedsecurityinfoa
+  if (group)
+    r = GetNamedSecurityInfoA(path.data(), SE_FILE_OBJECT, GROUP_SECURITY_INFORMATION, nullptr, &pSid, nullptr, nullptr, &pSD);
+  else
+    r = GetNamedSecurityInfoA(path.data(), SE_FILE_OBJECT, OWNER_SECURITY_INFORMATION, &pSid, nullptr, nullptr, nullptr, &pSD);
+
+  std::string s;
+  if(r == ERROR_SUCCESS)
+    s = fs_win32_get_owner(pSid);
+
+  LocalFree(pSD);
+  // https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-localfree
+  return s;
+}
+
+
 #else
 
 static std::optional<gid_t> fs_stat_uid(std::string_view path)
 {
-
   int r = 0;
 
 #if defined(STATX_UID) && defined(USE_STATX)
@@ -100,21 +124,8 @@ std::string
 fs_get_owner_name(std::string_view path)
 {
 #if defined(_WIN32)
-// https://learn.microsoft.com/en-us/windows/win32/secauthz/finding-the-owner-of-a-file-object-in-c--
-
-  PSECURITY_DESCRIPTOR pSD = nullptr;
-  PSID pSid = nullptr;
-  std::string s;
-
-  // https://learn.microsoft.com/en-us/windows/win32/api/aclapi/nf-aclapi-getnamedsecurityinfoa
-  if(GetNamedSecurityInfoA(path.data(), SE_FILE_OBJECT, OWNER_SECURITY_INFORMATION, &pSid, nullptr, nullptr, nullptr, &pSD) == ERROR_SUCCESS)
-    s = fs_win32_get_owner(pSid);
-
-  LocalFree(pSD);
-  // https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-localfree
-  if(!s.empty())
+  if (std::string s = fs_win32_owner(path, false); !s.empty())
     return s;
-
 #else
   if (auto uid = fs_stat_uid(path)) {
     if (auto pw = getpwuid(uid.value()))
@@ -131,20 +142,8 @@ std::string
 fs_get_owner_group(std::string_view path)
 {
 #if defined(_WIN32)
-  // use GetNamedSecurityInfoA to get group name
-
-  PSECURITY_DESCRIPTOR pSD = nullptr;
-  PSID pSid = nullptr;
-  std::string s;
-
-  // https://learn.microsoft.com/en-us/windows/win32/api/aclapi/nf-aclapi-getnamedsecurityinfoa
-  if(GetNamedSecurityInfoA(path.data(), SE_FILE_OBJECT, GROUP_SECURITY_INFORMATION, nullptr, &pSid, nullptr, nullptr, &pSD) == ERROR_SUCCESS)
-    s = fs_win32_get_owner(pSid);
-
-  LocalFree(pSD);
-  if(!s.empty())
+  if (std::string s = fs_win32_owner(path, true); !s.empty())
     return s;
-
 #else
   if (auto gid = fs_stat_gid(path)) {
     if (auto gr = getgrgid(gid.value()))
