590bc97797b6b6f933d074ee1516477f4d601c41
scivision <scivision@users.noreply.github.com>
2025-04-23 21:51:36 -0400

    GTest: IsSymlink

--- test/symlink/test_is_symlink.cpp
+++ test/symlink/test_is_symlink.cpp
@@ -1,44 +1,37 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-
 #include "ffilesystem.h"
-#include "ffilesystem_test.h"
+
+#include <gtest/gtest.h>
 
 
-int main(int argc, char* argv[]) {
+class TestSymlink : public testing::Test {
+  protected:
+    std::string tfile;
+    std::string tdir;
 
-if (argc < 3) {
-  std::cerr << "please give test link file and link dir\n";
-  return EXIT_FAILURE;
+    void SetUp() override {
+      std::vector<std::string> argvs = ::testing::internal::GetArgvs();
+      ASSERT_GE(argvs.size(), 3) << "please give test link file and link dir as arguments";
+
+      int argc = argvs.size();
+
+      tfile = argvs[argc-2];
+      ASSERT_TRUE(fs_is_file(tfile)) << tfile << " is not a file";
+
+      tdir = argvs[argc-1];
+      ASSERT_TRUE(fs_is_dir(tdir)) << tdir << " is not a directory";
+    }
+};
+
+
+TEST_F(TestSymlink, IsSymlinkFile){
+
+EXPECT_FALSE(fs_is_symlink("not-exist-file"));
+
+EXPECT_FALSE(fs_is_symlink(""));
+
+EXPECT_TRUE(fs_is_symlink(tfile));
 }
 
-const std::string tfile = argv[1];
-const std::string tdir = argv[2];
-
-if (fs_is_symlink("not-exist-file"))
-  err("is_symlink() should be false for non-existent file");
-
-if(fs_is_symlink(""))
-  err("is_symlink() should be false for empty file");
-
-if(!fs_is_file(tfile))
-  err("is_file(" + tfile + ") should be true for symlink file");
-
-if(!fs_is_symlink(tfile))
-  err("is_symlink(" + tfile + ") should be true for symlink file");
-
-std::cout << "PASS: file is_symlink(" << tfile << ")\n";
-
-if (!fs_is_dir(tdir))
-  err("is_dir(" + tdir + ") should be true for symlink directory");
-
-if (!fs_is_symlink(tdir))
-  err("is_symlink(" + tdir + ") should be true for symlink directory");
-
-std::cout << "PASS: directory is_symlink(" << tdir << ")\n";
-
-ok_msg("is_symlink C++");
-
-return EXIT_SUCCESS;
+TEST_F(TestSymlink, IsSymlinkDir){
+  EXPECT_TRUE(fs_is_symlink(tdir));
 }
