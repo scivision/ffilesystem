0efcbab84a2c586da5a530389a276ff71d868d8b
scivision <scivision@users.noreply.github.com>
2025-05-01 22:42:09 -0400

    test:Same: categories

--- test/core/test_equivalent.cpp
+++ test/core/test_equivalent.cpp
@@ -8,25 +8,29 @@
 class TestSame : public testing::Test {
     protected:
       std::string cwd;
+      std::string self_name;
       void SetUp() override {
         cwd = fs_as_posix(::testing::UnitTest::GetInstance()->original_working_dir());
+        std::vector<std::string> argvs = ::testing::internal::GetArgvs();
+        self_name = fs_file_name(argvs[0]);
       }
 };
 
-TEST_F(TestSame, Equivalent)
+TEST_F(TestSame, FileName)
 {
-std::string s1 = "../" + fs_file_name(cwd);
-std::string s2 = "./" + s1;
+EXPECT_TRUE(fs_equivalent(self_name, "./" + self_name));
+}
 
-ASSERT_TRUE(fs_is_dir(s1));
-EXPECT_TRUE(fs_is_dir(s2));
+TEST_F(TestSame, NotExist)
+{
+std::string s = "ffs_equiv_not-exist";
+EXPECT_FALSE(fs_equivalent(s, s));
+}
 
-EXPECT_TRUE(fs_equivalent(s1, s2));
+
+TEST_F(TestSame, Relative)
+{
 EXPECT_TRUE(fs_equivalent("..", fs_parent(cwd)));
 EXPECT_TRUE(fs_equivalent(".", "./"));
 EXPECT_TRUE(fs_equivalent(".", cwd));
-
-s1 = "ffs_equiv_not-exist";
-EXPECT_FALSE(fs_equivalent(s1, s1));
-
 }
