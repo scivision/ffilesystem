b537a72810dcc1186ed5a5a9d1e17b7e399f4a3c
scivision <scivision@users.noreply.github.com>
2025-05-06 00:38:24 -0400

    remove:windows: wide

--- src/move.cpp
+++ src/move.cpp
@@ -36,12 +36,13 @@ fs_remove(std::string_view path)
   // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-deletefilea
   // may need these when deleting a symlink
   // https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-createsymboliclinka#remarks
+  std::wstring w = fs_win32_to_wide(path);
 
   if(fs_is_file(path)){
-    if(DeleteFileA(path.data()) != 0)
+    if(DeleteFileW(w.data()) != 0)
       return true;
   } else if(fs_is_dir(path)){
-    if(RemoveDirectoryA(path.data()) != 0)
+    if(RemoveDirectoryW(w.data()) != 0)
       return true;
   }
 
