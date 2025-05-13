02132a344cfbb46860ca53a28048c986c07c3d6d
scivision <scivision@users.noreply.github.com>
2025-05-13 16:38:20 -0400

    test:stem: add spaces, UNC

--- test/core/test_filename.cpp
+++ test/core/test_filename.cpp
@@ -41,6 +41,49 @@ EXPECT_EQ(fs_file_name(R"(\\?\C:\)"), "");
 EXPECT_EQ(fs_file_name(R"(\\.\C:\)"), "");
 EXPECT_EQ(fs_file_name(R"(\\?\UNC\server\share)"), "share");
 EXPECT_EQ(fs_file_name(R"(\\?\UNC\server\share\日本語)"), "日本語");
-EXPECT_EQ(fs_file_name(R"(\\server\share\some space here)"), "some space here");
-EXPECT_EQ(fs_file_name(R"(\\?\C:\some space here)"), "some space here");
+EXPECT_EQ(fs_file_name(R"(\\server\share\some space here.txt)"), "some space here.txt");
+EXPECT_EQ(fs_file_name(R"(\\?\C:\some space here.txt)"), "some space here.txt");
+}
+
+
+TEST(TestStem, Agnostic)
+{
+
+EXPECT_EQ(fs_stem(""), "");
+EXPECT_EQ(fs_stem("stem.a.b"), "stem.a");
+EXPECT_EQ(fs_stem("stem.a"), "stem");
+EXPECT_EQ(fs_stem("stem"), "stem");
+EXPECT_EQ(fs_stem(".stem"), ".stem");
+EXPECT_EQ(fs_stem("stem."), "stem");
+EXPECT_EQ(fs_stem("stem.a."), "stem.a");
+EXPECT_EQ(fs_stem("stem/a/b"), "b");
+EXPECT_EQ(fs_stem("./.stem"), ".stem");
+EXPECT_EQ(fs_stem("../.stem"), ".stem");
+EXPECT_EQ(fs_stem(".stem.txt"), ".stem");
+EXPECT_EQ(fs_stem("a/.."), "..");
+EXPECT_EQ(fs_stem("a/../"), "");
+EXPECT_EQ(fs_stem("a/."), ".");
+
+EXPECT_EQ(fs_stem("日本語.日本語"), "日本語");
+EXPECT_EQ(fs_stem("some space.txt"), "some space");
+EXPECT_EQ(fs_stem("some space"), "some space");
+}
+
+TEST(TestStem, Windows)
+{
+if(!fs_is_windows())
+  GTEST_SKIP() << "Windows only test";
+
+EXPECT_EQ(fs_stem(R"(C:\a\ball.text)"), "ball");
+
+EXPECT_EQ(fs_stem(R"(\\?\)"), "");
+EXPECT_EQ(fs_stem(R"(\\.\)"), "");
+
+EXPECT_EQ(fs_stem(R"(\\?\C:\)"), "");
+EXPECT_EQ(fs_stem(R"(\\.\C:\)"), "");
+EXPECT_EQ(fs_stem(R"(\\?\UNC\server\share)"), "share");
+EXPECT_EQ(fs_stem(R"(\\?\UNC\server\share\日本語.txt)"), "日本語");
+
+EXPECT_EQ(fs_stem(R"(\\server\share\some space here.txt)"), "some space here");
+EXPECT_EQ(fs_stem(R"(\\?\C:\some space here.txt)"), "some space here");
 }
