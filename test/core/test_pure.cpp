3320c55571001c222f4dc2d1b7135d9b37f97e1d
scivision <scivision@users.noreply.github.com>
2025-05-13 16:43:06 -0400

    test:suffix: add UNC

--- test/core/test_pure.cpp
+++ test/core/test_pure.cpp
@@ -87,3 +87,43 @@ EXPECT_EQ(fs_stem(R"(\\?\UNC\server\share\日本語.txt)"), "日本語");
 EXPECT_EQ(fs_stem(R"(\\server\share\some space here.txt)"), "some space here");
 EXPECT_EQ(fs_stem(R"(\\?\C:\some space here.txt)"), "some space here");
 }
+
+TEST(TestSuffix, Agnostic)
+{
+EXPECT_EQ(fs_suffix(""), "");
+EXPECT_EQ(fs_suffix("a"), "");
+EXPECT_EQ(fs_suffix("a."), ".");
+EXPECT_EQ(fs_suffix("a.b"), ".b");
+EXPECT_EQ(fs_suffix("a.b.c"), ".c");
+EXPECT_EQ(fs_suffix("a/b.c"), ".c");
+EXPECT_EQ(fs_suffix("a/b.c.d"), ".d");
+EXPECT_EQ(fs_suffix("a/b/c.d"), ".d");
+EXPECT_EQ(fs_suffix("a/b/c.d.e"), ".e");
+EXPECT_EQ(fs_suffix("a/b/c.d/e"), "");
+EXPECT_EQ(fs_suffix(".a"), "");
+EXPECT_EQ(fs_suffix(".a."), ".");
+EXPECT_EQ(fs_suffix(".a.b"), ".b");
+EXPECT_EQ(fs_suffix("./b.c"), ".c");
+EXPECT_EQ(fs_suffix("../.b.c"), ".c");
+EXPECT_EQ(fs_suffix("日本語.語"), ".語");
+EXPECT_EQ(fs_suffix("some space.txt"), ".txt");
+}
+
+TEST(TestSuffix, Windows)
+{
+if(!fs_is_windows())
+  GTEST_SKIP() << "Windows only test";
+
+EXPECT_EQ(fs_suffix(R"(C:\a\ball.text)"), ".text");
+
+EXPECT_EQ(fs_suffix(R"(\\?\)"), "");
+EXPECT_EQ(fs_suffix(R"(\\.\)"), "");
+
+EXPECT_EQ(fs_suffix(R"(\\?\C:\)"), "");
+EXPECT_EQ(fs_suffix(R"(\\.\C:\)"), "");
+EXPECT_EQ(fs_suffix(R"(\\?\UNC\server\share)"), "");
+EXPECT_EQ(fs_suffix(R"(\\?\UNC\server\share\日本語.txt)"), ".txt");
+
+EXPECT_EQ(fs_suffix(R"(\\server\share\some space here.txt)"), ".txt");
+EXPECT_EQ(fs_suffix(R"(\\?\C:\some space here.txt)"), ".txt");
+}
