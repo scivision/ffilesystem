fd08a9a24723cb942ef608b1a41ec4296c8b7204
scivision <scivision@users.noreply.github.com>
2025-05-13 14:08:11 -0400

    print_error: cleanup printing

--- src/log.cpp
+++ src/log.cpp
@@ -18,11 +18,11 @@ static void fs_emit_error()
 {
 #if defined(_WIN32) || defined(__CYGWIN__)
   if (DWORD error = GetLastError(); error)
-    std::cerr << "GetLastError:" << std::system_category().message(error) << " ";
+    std::cerr << "GetLastError: " << std::system_category().message(error) << " ";
 #else
   if(errno){
     auto econd = std::generic_category().default_error_condition(errno);
-    std::cerr << "errno:" << econd.message() << " ";
+    std::cerr << "errno: " << econd.message() << "\n";
   }
 #endif
 }
@@ -31,43 +31,35 @@ static void fs_emit_error()
 void fs_print_error(std::string_view path, std::string_view fname)
 {
 
-  std::cerr << "ERROR: Ffilesystem:" << fname << "(" << path << ")  ";
+  std::cerr << "ERROR: Ffilesystem: " << fname << "(" << path << ")\n";
 
   fs_emit_error();
-
-  std::cerr << std::endl;
 }
 
 
 void fs_print_error(std::string_view path, std::string_view fname, const std::error_code& ec)
 {
-  std::cerr << "ERROR: Ffilesystem:" << fname << "(" << path << ")  ";
+  std::cerr << "ERROR: Ffilesystem: " << fname << "(" << path << ")\n";
   if(ec)
-    std::cerr << ec.message();
+    std::cerr << "C++ exception: " << ec.message() << " " << ec.value() << "\n";
 
   fs_emit_error();
-
-  std::cerr << std::endl;
 }
 
 
 void fs_print_error(std::string_view path1, std::string_view path2, std::string_view fname)
 {
-  std::cerr << "ERROR: Ffilesystem:" << fname << "(" << path1 <<  ", " << path2 << ")  ";
+  std::cerr << "ERROR: Ffilesystem: " << fname << "(" << path1 <<  ", " << path2 << ")\n";
 
   fs_emit_error();
-
-  std::cerr << std::endl;
 }
 
 void fs_print_error(std::string_view path1, std::string_view path2, std::string_view fname, const std::error_code& ec)
 {
-  std::cerr << "ERROR: Ffilesystem:" << fname << "(" << path1 <<  ", " << path2 << ")  ";
+  std::cerr << "ERROR: Ffilesystem: " << fname << "(" << path1 <<  ", " << path2 << ")\n";
 
   if(ec)
-    std::cerr << "C++ exception:" << ec.message() <<  " " << ec.value() << " ";
+    std::cerr << "C++ exception: " << ec.message() <<  " " << ec.value() << "\n";
 
   fs_emit_error();
-
-  std::cerr << std::endl;
 }
