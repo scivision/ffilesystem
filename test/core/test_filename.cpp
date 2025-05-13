c5982b887b1a3e1c24eb823ae9d6923423a2e549
scivision <scivision@users.noreply.github.com>
2025-05-13 16:34:31 -0400

    test:filename: spaces and UNC

--- test/core/test_filename.cpp
+++ test/core/test_filename.cpp
@@ -18,6 +18,7 @@ EXPECT_EQ(fs_file_name("a/.."), "..");
 EXPECT_EQ(fs_file_name("a/b"), "b");
 EXPECT_EQ(fs_file_name("a/b/"), "");
 EXPECT_EQ(fs_file_name("a/b/c"), "c");
+EXPECT_EQ(fs_file_name("a/b sdc/some space"), "some space");
 EXPECT_EQ(fs_file_name("ab/.parent"), ".parent");
 EXPECT_EQ(fs_file_name("ab/.parent.txt"), ".parent.txt");
 EXPECT_EQ(fs_file_name("a/b/../.parent.txt"), ".parent.txt");
@@ -27,10 +28,19 @@ EXPECT_EQ(fs_file_name("./日本語"), "日本語");
 
 TEST(TestFilename, Windows)
 {
+if(!fs_is_windows())
+  GTEST_SKIP() << "Windows only test";
 
-  if(!fs_is_windows())
-    GTEST_SKIP() << "Windows only test";
+EXPECT_EQ(fs_file_name("C:/"), "");
+EXPECT_EQ(fs_file_name(R"(C:\ab\asb)"), "asb");
 
-  EXPECT_EQ(fs_file_name("C:/"), "");
-  EXPECT_EQ(fs_file_name(R"(C:\ab\asb)"), "asb");
+EXPECT_EQ(fs_file_name(R"(\\?\)"), "");
+EXPECT_EQ(fs_file_name(R"(\\.\)"), "");
+
+EXPECT_EQ(fs_file_name(R"(\\?\C:\)"), "");
+EXPECT_EQ(fs_file_name(R"(\\.\C:\)"), "");
+EXPECT_EQ(fs_file_name(R"(\\?\UNC\server\share)"), "share");
+EXPECT_EQ(fs_file_name(R"(\\?\UNC\server\share\日本語)"), "日本語");
+EXPECT_EQ(fs_file_name(R"(\\server\share\some space here)"), "some space here");
+EXPECT_EQ(fs_file_name(R"(\\?\C:\some space here)"), "some space here");
 }
