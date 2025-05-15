fd01b46109719897419100a11f8490a11d133660
scivision <scivision@users.noreply.github.com>
2025-05-15 11:05:32 -0400

    test:copy_file: use <filesystem>, add tests

--- test/core/test_copyfile.cpp
+++ test/core/test_copyfile.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <fstream>
 #include <cstdint>
+#include <filesystem>
 
 #include "ffilesystem.h"
 
@@ -10,13 +11,29 @@
 class TestCopyFile : public testing::Test {
   protected:
     std::string s1, s2, s3, s4, t1;
+    std::string ext1, ext5;
     std::uintmax_t iref;
 
     void SetUp() override {
-      s1 = "ffs_dummy_cpp.txt";
-      s2 = "ffs_dummy_cpp.txt.copy";
-      s3 = "ffs_empty_copyfile.txt";
-      s4 = "ffs_empty_copyfile_copy.txt";
+      auto cwd = std::filesystem::current_path();
+
+      auto inst = testing::UnitTest::GetInstance();
+      auto info = inst->current_test_info();
+
+      // https://google.github.io/googletest/reference/testing.html#UnitTest::current_test_suite
+      std::string test_name_ = info->name();
+      std::string test_suite_name_ = info->test_suite_name();
+      std::string n = test_suite_name_ + "-" + test_name_;
+
+      s1 = (cwd / (n + "_some_text.txt")).string();
+      s2 = (cwd / (n + "_some_text.txt.copy")).string();
+      s3 = (cwd / (n + "_empty.txt")).string();
+      s4 = (cwd / (n + "_empty.txt.copy")).string();
+      if(fs_is_windows()){
+        ext1 = R"(\\?\)" + s1;
+        ext5 = R"(\\?\)" + s2 + ".long";
+      }
+
       t1 = "及せゃ市人購ゅトてへ投際ト点吉で速流つ今日";
 
       // Write to the first file
@@ -25,39 +42,59 @@ class TestCopyFile : public testing::Test {
       ofs << t1;
       ofs.close();
 
-      iref = fs_file_size(s1);
+      iref = std::filesystem::file_size(s1);
       ASSERT_NE(iref, 0);
+
+      std::cout << "TestCopyFile::SetUp: " << s1 << " bytes " << iref << "\n";
 
       ASSERT_TRUE(fs_touch(s3));
     }
+
     void TearDown() override {
-      fs_remove(s1);
-      fs_remove(s2);
-      fs_remove(s3);
-      fs_remove(s4);
+      std::filesystem::remove(s1);
+      std::filesystem::remove(s2);
+      std::filesystem::remove(s3);
+      std::filesystem::remove(s4);
+      if(fs_is_windows()){
+        std::filesystem::remove(ext5);
+      }
     }
 };
 
 TEST_F(TestCopyFile, CopyFile){
 
-  std::string t2;
+std::string t2;
 
-  // Copy the file
-  EXPECT_TRUE(fs_copy_file(s1, s2, true));
-  EXPECT_TRUE(fs_is_file(s2));
+// Copy the file
+EXPECT_TRUE(fs_copy_file(s1, s2, false));
+EXPECT_TRUE(std::filesystem::is_regular_file(s2));
+EXPECT_FALSE(fs_copy_file(s1, s2, false)); // Should fail since s2 already exists
 
-  EXPECT_EQ(fs_file_size(s2), iref);
+EXPECT_EQ(std::filesystem::file_size(s2), iref);
 
-  // Read from the copied file
-  std::ifstream ifs(s2);
-  std::getline(ifs, t2);
-  ifs.close();
+// Read from the copied file
+std::ifstream ifs(s2);
+std::getline(ifs, t2);
+ifs.close();
 
-  EXPECT_EQ(t1, t2);
+EXPECT_EQ(t1, t2);
 
-  EXPECT_TRUE(fs_copy_file(s3, s4, true));
-  EXPECT_TRUE(fs_is_file(s4));
+EXPECT_TRUE(fs_copy_file(s3, s4, true));
+EXPECT_TRUE(std::filesystem::is_regular_file(s4));
 
-  EXPECT_EQ(fs_file_size(s4), 0);
+EXPECT_EQ(std::filesystem::file_size(s4), 0);
+
+}
+
+
+TEST_F(TestCopyFile, Windows){
+  if(!fs_is_windows())
+    GTEST_SKIP() << "Windows specific test";
+
+
+EXPECT_TRUE(fs_copy_file(ext1, ext5, false));
+EXPECT_TRUE(std::filesystem::is_regular_file(ext5));
+EXPECT_EQ(std::filesystem::file_size(ext5), iref);
+
 
 }
