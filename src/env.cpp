252bc196f465c79ee60ccf7fc43911a283c0b3b9
scivision <scivision@users.noreply.github.com>
2025-06-09 12:14:40 -0400

    unix:setenv: use unsetenv if value is empty

--- src/env.cpp
+++ src/env.cpp
@@ -35,16 +35,20 @@ std::optional<std::string> fs_getenv(std::string_view name)
 
 bool fs_setenv(std::string_view name, std::string_view value)
 {
+  // if value is empty, remove the variable from the environment
 
 #if defined(_WIN32)
   // SetEnvironmentVariable returned OK but set blank values
   // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/putenv-wputenv
 
-  if(_putenv_s(name.data(), value.data()) == 0)  FFS_LIKELY
+  if(_putenv_s(name.data(), value.data()) == 0)
     return true;
 #else
   // https://www.man7.org/linux/man-pages/man3/setenv.3.html
-  if(setenv(name.data(), value.data(), 1) == 0)  FFS_LIKELY
+  if(value.empty()) {
+    if(unsetenv(name.data()) == 0)
+      return true;
+  } else if(setenv(name.data(), value.data(), 1) == 0)
     return true;
 #endif
 
