bf5f3b5bf563cd4579dee663a7898533175b58ca
scivision <scivision@users.noreply.github.com>
2026-03-24 22:02:39 -0400

    test:fortran:realpath: improve/generalize test

--- test/core/test_realpath.f90
+++ test/core/test_realpath.f90
@@ -10,17 +10,16 @@ valgrind : block
 
 character(:), allocatable :: p1, p2
 
-integer :: L1, L2
+integer :: L1
 
 ! -- current directory  -- old MacOS doesn't handle "." or ".." alone
 p1 = realpath(".")
 L1 = len_trim(p1)
 if(L1 == 0) error stop "FAILED: realpath '.'"
 
-p2 = get_cwd()
-L2 = len_trim(p2)
-if(L1 /= L2) then
-  write(stderr, '(a,1x,i0,1x,i0)')  "FAILED: realpath '.' failed: " // p1 // " /= " // p2, L1, L2
+p2 = realpath(get_cwd())
+if(p1 /= p2) then
+  write(stderr, '(a)')  "FAILED: realpath '.' failed: " // p1 // " /= " // p2
   error stop
 endif
 
@@ -30,9 +29,9 @@ print *, "OK: current dir = ", p1
 ! -- relative dir
 p1 = realpath("..")
 
-L2 = len_trim(p1)
-if (L2 >= L1) then
-  write(stderr,'(a,1x,i0,1x,i0)') 'ERROR:realpath:relative: up dir not realpath: ~/.. => ' // p1, L1, L2
+p2 = parent(p2)
+if (p1 /= p2) then
+  write(stderr,'(a)') 'ERROR:realpath:relative: up dir not realpath ' // p1 // " /= " // p2
   error stop
 end if
 print *, 'OK: realpath(..) = ', p1
