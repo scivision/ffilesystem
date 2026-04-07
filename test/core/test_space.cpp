4ab4abb85bf0724f4d4d194258842613772b9da8
scivision <scivision@users.noreply.github.com>
2026-04-07 19:30:15 -0400

    space: handle non-null term input

--- test/core/test_space.cpp
+++ test/core/test_space.cpp
@@ -1,17 +1,45 @@
 #include "ffilesystem.h"
+#include <iostream>
 
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using ::testing::AllOf;
+using ::testing::Gt;
+using ::testing::Lt;
 
 // for Windows need an invalid drive as a non-existing relative path just gives the space anyway.
 
-TEST(TestSpace, SpaceAvailable)
+class TestSpace : public testing::Test {
+  protected:
+    std::string dir, in_dir;
+    std::string_view nonnull_dir, nonnull_file;
+    std::uintmax_t bad = -1;
+
+    void SetUp() override {
+      dir = fs_drop_slash(::testing::TempDir());
+      std::cout << "Testing space on backend " << fs_backend() << " with temp dir " << dir << "\n";
+
+      in_dir = dir + "-invalid-memory-trailing-non-null-terminated-string_view";
+      nonnull_dir = std::string_view(in_dir.data(), dir.size());
+      ASSERT_NE(nonnull_dir.back(), '\0') << "nonnull_dir should not be null-terminated\n";
+    }
+};
+
+TEST_F(TestSpace, SpaceAvailable)
 {
-EXPECT_GT(fs_space_available(::testing::TempDir()), 0);
-EXPECT_EQ(fs_space_available("cc:/not-exist-available"), static_cast<std::uintmax_t>(-1)) << "backend " << fs_backend();
+EXPECT_THAT(fs_space_available(dir), AllOf(Gt(0), Lt(bad)));
+
+EXPECT_EQ(fs_space_available("cc:/not-exist-available"), bad) << "backend " << fs_backend();
+
+EXPECT_NE(fs_space_available(nonnull_dir), bad) << "problem with non null-terminated path " << nonnull_dir;
 }
 
-TEST(TestSpace, SpaceCapacity)
+TEST_F(TestSpace, SpaceCapacity)
 {
-EXPECT_GT(fs_space_capacity(::testing::TempDir()), 0);
-EXPECT_EQ(fs_space_capacity("cc:/not-exist-capacity"), static_cast<std::uintmax_t>(-1)) << "backend " << fs_backend();
+EXPECT_THAT(fs_space_capacity(dir), AllOf(Gt(0), Lt(bad)));
+
+EXPECT_EQ(fs_space_capacity("cc:/not-exist-capacity"), bad) << "backend " << fs_backend();
+
+EXPECT_NE(fs_space_capacity(nonnull_dir), bad) << "problem with non null-terminated path " << nonnull_dir;
 }
