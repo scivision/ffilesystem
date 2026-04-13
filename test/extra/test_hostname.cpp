24399793a3e30c92e9aad271d74b908887302420
scivision <scivision@users.noreply.github.com>
2026-04-13 14:01:48 -0400

    remove dupe test

--- test/extra/test_hostname.cpp
+++ test/extra/test_hostname.cpp
@@ -13,17 +13,6 @@ TEST(TestHostname, Hostname){
   EXPECT_NE(s.length(), fs_get_max_path()) << "hostname length is equal to max path length";
 }
 
-TEST(TestExe, ExePath){
-  std::string p = fs_exe_path();
-  ASSERT_FALSE(p.empty());
-
-  std::vector<std::string> argvs = ::testing::internal::GetArgvs();
-
-  std::string n = fs_file_name(p);
-  EXPECT_TRUE(fs_equivalent(p, argvs[0])) << "executable path is not equivalent to the current executable";
-}
-
-
 TEST(TestMax, MaxComponent){
   EXPECT_GE(fs_max_component("/"), 1);
 }
